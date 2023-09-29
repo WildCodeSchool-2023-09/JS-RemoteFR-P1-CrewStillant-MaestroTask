031d361c3d442170d3e6b5e1c2ee1374cd9ec9fe
BAYRAM TUGBA <tugbabayram69@gmail.com>
2023-09-29 16:29:27 +0200

    j'ai fini le header presque

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
-
-// le menu déroulant en appuyant sur le bouton Ajouter une checklist
-const dropdownBtn = document.querySelector(".dropdown-btn");
-
-
-// Fonction pour gérer l'affichage du menu déroulant
-function toggleDropdownMenu() {
-    dropdownMenu.classList.toggle("visible");
-}
-
+//Tugba
 // Fonction pour afficher la date et l'heure
 function dateAndTime() {
-    const currentDateAndTimeElements = document.querySelectorAll(".currentDateAndTime");
+    const currentDateElement = document.querySelector(".currentDate");
+    const currentTimeElement = document.querySelector(".currentTime");
 
     const currentDate = new Date();
-    const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const optionsTime = { hour: '2-digit', minute: '2-digit' }; 
+    const optionsDate = { weekday: 'long', month: 'long', day: 'numeric' };
+    const optionsTime = { hour: '2-digit', minute: '2-digit'}; 
 
     const formattedDate = currentDate.toLocaleDateString('fr-FR', optionsDate);
     const formattedTime = currentDate.toLocaleTimeString('fr-FR', optionsTime);
 
-    currentDateAndTimeElements.forEach(element => {
-        element.textContent = `${formattedDate} ${formattedTime}`;
-    });
+    currentDateElement.textContent = formattedDate;
+    currentTimeElement.textContent = formattedTime;
 }
 
 // Appeler la fonction d'affichage de la date et de l'heure
@@ -30,16 +21,20 @@ dateAndTime();
 // Mettre à jour la date et l'heure régulièrement
 setInterval(dateAndTime, 1000);
 
+// Le clic pour les emojis
+const emojis = document.querySelectorAll('.mood img');
 
+// Gestionnaire d'événement de clic pour chaque emoji
+emojis.forEach(emoji => {
+    emoji.addEventListener('click', () => {
+        // Réinitialiser tous les emojis en gris
+        emojis.forEach(e => e.classList.add('grayed'));
 
-// le menu déroulant en appuyant sur le bouton Ajouter une checklist
-const dropdownBtn = document.querySelector(".dropdown-btn");
-const dropdownMenu = document.querySelector(".dropdown-menu-content");
+        // Retirer l'effet gris de l'emoji cliqué
+        emoji.classList.remove('grayed');
+    });
+});
 
-// Fonction pour gérer l'affichage du menu déroulant
-function toggleDropdownMenu() {
-    dropdownMenu.classList.toggle("visible");
-}
 
 //QUENTIN
 // le menu déroulant en appuyant sur le bouton Ajouter une checklist
@@ -65,29 +60,18 @@ inputToDo.value = "";
 };
 
 // Déclaration de Post-Its, la div qui prendra les autres posts its
-
 const postItsgroupe = document.querySelector(".postits");
 
 // function création du premier post-it
-
 const createPostItNormal = () => {
 const postit = document.createElement("div");
 postit.classList.add("post-it");
 postItsgroupe.appendChild(postit);
 }
 //créer une zone de textarea
-
 //sur survol, afficher "vous pouvez cliquer ici pour rajouter une tache"
-
 //sur click, prendre le texte saisi comme value et le renvoyer dans une li rattaché un ul sur le div du post-it 
-
 // ce ul ne s'affiche que si il à au moins une valeur (donc .visible à utliser avec un if true else)
-
-
-}
-
 // a la fin de la variable créer une const Listecrée = true
 // et se rapporter au début du code de la fonction pour mettre un if true afficher message "plus de place pour cette liste ;)"
 // else lancer le reste
-
-
